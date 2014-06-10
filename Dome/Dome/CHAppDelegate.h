eced020848efdb7102870121f8f8a87d09f0e363
FeiTianZhu858 <lanou3g@chuanzhang.local>
2014-06-10 16:41:30 +0800

    Initial Commit

---
+++ Dome/Dome/CHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CHAppDelegate.h
+//  Dome
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
